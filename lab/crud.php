9b9a89b369b82dcf861a470c9ddf84f303354981
paschalk <64302966+paschalk@users.noreply.github.com>
2020-05-17 20:56:18 +0300

    Add files via upload

--- lab/crud.php
+++ lab/crud.php
@@ -1,11 +1,15 @@
-<?php  
-interface Crud{
-	public function save();
-	public function readAll();
-	public function readUnique();
-	public function search();
-	public function update();
-	public function removeOne();
-	public function removeAll();
-}
+<?php
+   interface Crud{
+
+    public function save($conn);
+    public function readAll();
+    public function readUnique();
+    public function search();
+    public function update();
+    public function removeOne();
+    public function removeAll();
+
+    public function valiteForm();
+    public function createFormErrorSessions();
+   }
 ?>
